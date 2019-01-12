b136488c2466be3b648539ee9c28f149b03abc67
AlHarlow <AlHarlow@users.noreply.github.com>
2019-01-11 23:00:13 -0600

    Add files via upload

---
+++ PDFtest/PDFtest/Module1.vb
@@ -0,0 +1,13 @@
+﻿Imports System
+Module Module1
+
+    Sub Main()
+        Dim base64Encoded As String = "YmFzZTY0IGVuY29kZWQgc3RyaW5n"
+        Dim base64Decoded As String
+        Dim data() As Byte = System.Convert.FromBase64String(base64Encoded)
+        base64Decoded = System.Text.ASCIIEncoding.ASCII.GetString(data)
+
+        Console.Read()
+    End Sub
+
+End Module
